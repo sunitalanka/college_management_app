b89db7ae23fc9fd2a87cd50b74c38ea84d664bd7
Sunita Lanka <sunita.uscareer@gmail.com>
2018-08-04 16:08:04 -0500

    initial commit

---
+++ src/main/java/org/launchcode/controllers/StudentController.java
@@ -0,0 +1,16 @@
+package org.launchcode.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class StudentController {
+
+    @RequestMapping(value="")
+    @ResponseBody
+    public String index(){
+
+        return "Hello College Management System";
+    }
+}
